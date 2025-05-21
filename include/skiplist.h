a7915211bbe9571cdfeae6c999540616edc426e7
vinch <a478honor0109@gmail.com>
2025-05-21 10:41:01 +0300

    initial commit

---
+++ include/skiplist.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <utility>
+
+using std::size_t;
+
---
+++ test/test_skiplist.cpp
@@ -0,0 +1,8 @@
+#include <gtest.h>
+#include "skiplist.h"
+
+TEST(SkiplistTest, can_create_skiplist)
+{
+    ADD_FAILURE();
+}
+
